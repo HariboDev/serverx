cc3e34002b2bcdd13f17a7989473b6c4bca27ed2
HendersonC <conor.henderson@fujitsu.com>
2022-03-21 20:48:58 +0000

    Split the gets out into seperate files

--- src/utils/get-gcp.ts
+++ src/utils/get-gcp.ts
@@ -1,50 +1,82 @@
-const compute = require("@google-cloud/compute");
+import Compute, { InstancesClient } from "@google-cloud/compute";
+import { Config } from "@oclif/core";
+import { FlagInput } from "@oclif/core/lib/interfaces";
 import chalk from "chalk";
 import getZones from "./get-zones";
-import { IInstancesData } from "./interfaces";
-import { isPortReachable } from "./utils";
-// import { isPortReachable, readJsonFile, writeJsonFile, getEnabledRegions, getCurrentIp } from "./utils";
-// import { IConfigData, IInstancesData, IAccountCredentials, IInstance } from "./interfaces";
-// import { FlagInput } from "@oclif/core/lib/interfaces";
-// import { Config } from "@oclif/core";
+import { IConfigData, IInstance, IInstancesData } from "./interfaces";
+import { isPortReachable, readJsonFile, writeJsonFile } from "./utils";
 
-export default async function getGCP(flags: any): Promise<any> {
-  console.log(`${chalk.green("[INFO]")} Gathering GCP managed instances`);
-  console.log(flags.region.toString().split(","));
-  if (flags.region.toString() === "all") {
-    console.log("hello");
+export default async function getGCP(flags: FlagInput<any>, config: Config): Promise<any> {
+  const configData: IConfigData = await readJsonFile(config.configDir, "config");
+
+  if (!configData) {
+    return;
   }
 
-  let instancesList: any = [];
-  let instances: any = [];
-  instancesList.self = []
-  instancesList.gcp = []
-  flags.region.toString() === "all" ? instances = await listAllInstances("new-project-12365") : instances = await listInstanceByZone("new-project-12365", flags.region.toString().split(","));
-  instancesList.gcp = [...instancesList.gcp, ...instances]
-  console.log(instancesList)
-  return instancesList;
+  const instancesData: IInstancesData = await readJsonFile(config.dataDir, "instances");
+
+  if (!instancesData) {
+    return;
+  }
+
+  if (JSON.parse(flags["no-refresh"].toString())) {
+    const instances: IInstancesData = {
+      aws: [],
+      gcp: instancesData.gcp.filter((instance: IInstance) => {
+        return ((flags.region.toString() === "all" ||
+        (flags.region.toString().includes(instance.location))) &&
+        (flags.state.toString() === "all" ||
+            flags.state.toString().split(",").includes(instance.state))
+        );
+      }),
+      self: []
+    };
+    return instances;
+  }
+
+  console.log(`${chalk.green("[INFO]")} Gathering GCP managed instances`);
+
+  flags.region.toString() === "all" ? instancesData.gcp = await listAllInstances("new-project-12365") : instancesData.gcp = await listInstanceByZone("new-project-12365", flags.region.toString().split(","));
+
+  const writeResponse: boolean = await writeJsonFile(config.dataDir, "instances", JSON.stringify(instancesData));
+
+  if (!writeResponse) {
+    return;
+  }
+
+  return instancesData;
 }
 
 async function listAllInstances(projectId: string): Promise<any> {
-  const instanceClient = new compute.InstancesClient();
+  const instanceClient: InstancesClient = new Compute.InstancesClient();
   const instancesData: any = [];
   let ephermeralIP: string = "";
+  let accessible: boolean = false;
   const instanceList = instanceClient.aggregatedListAsync({
     project: projectId,
-    maxResult: 5
+    maxResults: 5
   });
   for await (const [zone, instancesObject] of instanceList) {
     const instances = instancesObject.instances;
 
     if (instances && instances.length > 0) {
-      for (const instance of instances) {
-        if (instance.networkInterfaces[0].accessConfigs.length > 0) {
-          ephermeralIP = instance.networkInterfaces[0].accessConfigs[0].natIP;
-        } else {
-          ephermeralIP = "Unknown"
+      for await (const instance of instances) {
+        if (!instance.networkInterfaces || instance.networkInterfaces.length < 0) {
+          return;
         }
 
-        const accessible: boolean = await isPortReachable(22, { host: instance.address, timeout: 1000 });
+        if (!instance.networkInterfaces[0].accessConfigs || instance.networkInterfaces[0].accessConfigs.length < 0) {
+          ephermeralIP = "Unknown";
+        } else if (instance.networkInterfaces[0].accessConfigs.length > 0 && instance.networkInterfaces[0].accessConfigs[0].natIP) {
+          ephermeralIP = instance.networkInterfaces[0].accessConfigs[0].natIP;
+        } else {
+          ephermeralIP = "Unknown";
+        }
+
+        if (ephermeralIP !== "Unknown") {
+          accessible = await isPortReachable(22, { host: ephermeralIP, timeout: 1000 });
+        }
+
         instancesData.push({ name: instance.name, address: ephermeralIP, state: instance.status, location: zone, accessible: accessible });
       }
     }
@@ -54,24 +86,35 @@ async function listAllInstances(projectId: string): Promise<any> {
 }
 
 async function listInstanceByZone(projectId: string, regions: string[]): Promise<any> {
-  const instanceClient = new compute.InstancesClient();
+  const instanceClient: InstancesClient = new Compute.InstancesClient();
   const instancesData: any = [];
   let ephermeralIP: string = "";
+  let accessible: boolean = false;
   const zones = getZones(regions);
+
   for await (const zone of zones) {
     const instanceList = instanceClient.listAsync({
       project: projectId,
       zone: zone
     });
     for await (const instance of instanceList) {
-      console.log(instance);
-      if (instance.networkInterfaces[0].accessConfigs.length > 0) {
-        ephermeralIP = instance.networkInterfaces[0].accessConfigs[0].natIP;
+      if (!instance.networkInterfaces || instance.networkInterfaces.length < 0) {
+        return;
       }
 
-      const accessible: boolean = await isPortReachable(22, { host: instance.address, timeout: 1000 });
+      if (!instance.networkInterfaces[0].accessConfigs || instance.networkInterfaces[0].accessConfigs.length < 0) {
+        ephermeralIP = "Unknown";
+      } else if (instance.networkInterfaces[0].accessConfigs.length > 0 && instance.networkInterfaces[0].accessConfigs[0].natIP) {
+        ephermeralIP = instance.networkInterfaces[0].accessConfigs[0].natIP;
+      } else {
+        ephermeralIP = "Unknown";
+      }
+
+      if (ephermeralIP !== "Unknown") {
+        accessible = await isPortReachable(22, { host: ephermeralIP, timeout: 1000 });
+      }
+
       instancesData.push({ name: instance.name, address: ephermeralIP, state: instance.status, location: zone, accessible: accessible });
-      // console.log(instancesData);
     }
   }
 
---
+++ src/utils/get-self.ts
@@ -0,0 +1,52 @@
+import { Config } from "@oclif/core";
+import { FlagInput } from "@oclif/core/lib/interfaces";
+import chalk from "chalk";
+import { isPortReachable, readJsonFile } from "./utils";
+import { IConfigData, IInstance, IInstancesData } from "./interfaces";
+
+export default async function getSelf(flags: FlagInput<any>, config: Config): Promise<any> {
+  const configData: IConfigData = await readJsonFile(config.configDir, "config");
+
+  if (!configData) {
+    return;
+  }
+
+  const instancesData: IInstancesData = await readJsonFile(config.dataDir, "instances-self");
+
+  if (!instancesData) {
+    return;
+  }
+
+  if (JSON.parse(flags["no-refresh"].toString())) {
+    const instances: IInstancesData = {
+      self: instancesData.self.filter((instance: IInstance) => {
+        return (flags.region.toString() === "all" ||
+        (flags.region.toString().includes(instance.location)));
+      }),
+      aws: [],
+      gcp: []
+    };
+    return instances;
+  }
+
+  console.log(`${chalk.green("[INFO]")} Gathering self managed instances`);
+
+  const selfManagedInstances: Array<IInstance> = instancesData.self;
+  instancesData.self = [];
+
+  for await (const instance of selfManagedInstances) {
+    const instanceData = {
+      name: instance.name,
+      address: instance.address,
+      keyPair: instance.keyPair,
+      username: instance.username,
+      state: instance.state,
+      accessible: instance.accessible,
+      location: instance.location,
+      account: instance.account
+    };
+
+    instanceData.accessible = await isPortReachable(22, { host: instance.address, timeout: 1000 });
+    instancesData.self.push(instanceData);
+  }
+}
